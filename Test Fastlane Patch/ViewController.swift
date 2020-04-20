fd20dafa53cc76e686a913d754d087f01b08c823
John Mejia <john.mejia@blueapron.com>
2020-04-20 16:05:47 -0400

    initial commit

--- Test Fastlane Patch/ViewController.swift
+++ Test Fastlane Patch/ViewController.swift
@@ -9,10 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var versionLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let version = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as! String
+        versionLabel.text = version
     }
 
 
